75dd19a54fc0d5c5e65a8fa8cf0dcfe112f9d226
liangkhwai <liangkhwai@gmail.com>
2022-09-24 00:18:15 +0700

    delete old models

--- hotel/models.py
+++ hotel/models.py
@@ -3,32 +3,24 @@ from django.db import models
 
 # Create your models here.
 
+# class Accounts(models.Model):
+#     account_id = models.BigAutoField(primary_key=True,auto_created=True,serialize=False)
+#     type = models.CharField(max_length=255)
+#     username = models.CharField(max_length=255)
+#     password = models.CharField(max_length=255)
 
-class Guest(models.Model):
-    guest_id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='guest Id')
-    guest_name = models.CharField(max_length=60)
-    guest_email = models.EmailField(unique=True,max_length=255)
-    guest_address = models.TextField(max_length=255,null=True)
-    guest_tel = models.CharField(max_length=10)
-    guest_pwd = models.CharField(max_length=255)
+# class Customer(models.Model):
+#     cust_id = models.BigAutoField(primary_key=True,auto_created=True,serialize=False)
+#     account = models.ManyToOneRel(Accounts)
+#     name = models.CharField(max_length=255)
+#     age = models.CharField(max_length=255)
+#     gender = models.CharField(max_length=255)
+#     tel = models.CharField(max_length=255)
+#     address = models.TextField(11)
+
+# class Reservation
+
     
-    
-class Room(models.Model):
-    room_id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='room Id')
-    room_type = models.CharField(max_length=50)
-    room_price = models.IntegerField()
-    room_description = models.TextField(max_length=255,null=True)
-    room_img = models.ImageField()
-
-
-class Booking(models.Model):
-    booking_id = models.BigAutoField(auto_created=True,primary_key=True,serialize=False,verbose_name='booking Id')
-    guest_id = models.ForeignKey(Guest,on_delete=models.CASCADE)
-    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
-    booking_checkin = models.DateTimeField()
-    booking_checkout = models.DateTimeField()
-    booking_status = models.CharField(max_length=255)
-    booking_numOfpeople = models.IntegerField()
 
 
     
