032be6b184958e425bd59c53ed7b45eec28f2ff5
liangkhwai <liangkhwai@gmail.com>
2022-09-23 15:29:50 +0700

    create booking model

--- hotel/models.py
+++ hotel/models.py
@@ -1,11 +1,11 @@
-from itertools import chain
+
 from django.db import models
 
 # Create your models here.
 
 
 class Guest(models.Model):
-    guest_id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    guest_id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='guest Id')
     guest_name = models.CharField(max_length=60)
     guest_email = models.EmailField(unique=True,max_length=255)
     guest_address = models.TextField(max_length=255,null=True)
@@ -14,12 +14,21 @@ class Guest(models.Model):
     
     
 class Room(models.Model):
-    room_id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    room_id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='room Id')
     room_type = models.CharField(max_length=50)
     room_price = models.IntegerField()
     room_description = models.TextField(max_length=255,null=True)
     room_img = models.ImageField()
 
+
+class Booking(models.Model):
+    booking_id = models.BigAutoField(auto_created=True,primary_key=True,serialize=False,verbose_name='booking Id')
+    guest_id = models.CharField(max_length=10)
+    room_id = models.CharField(max_length=10)
+    booking_checkin = models.DateTimeField()
+    booking_checkout = models.DateTimeField()
+    booking_status = models.CharField(max_length=255)
+    booking_numOfpeople = models.IntegerField()
 
 
     
